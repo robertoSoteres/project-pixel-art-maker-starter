d4e5f59ea2228151c4553bda14cd6a779aadc44a
roberto.soteres <roberto.soteres@gmail.com>
2017-09-01 11:28:07 +0200

    primeros pasos

--- designs.js
+++ designs.js
@@ -1,5 +1,11 @@
 // Select color input
 // Select size input
+var boton = document.getElementById("send");
+console.log(boton);
+boton.addEventListener("click", function(){
+    console.log("entro en enviar");
+});
+
 
 // When size is submitted by the user, call makeGrid()
 
