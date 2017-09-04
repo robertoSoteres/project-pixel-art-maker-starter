e46396a8bad8b9b2045c1393bffc2ef97326f55a
roberto.soteres <roberto.soteres@gmail.com>
2017-09-04 20:09:22 +0200

    final project. Part 1

--- designs.js
+++ designs.js
@@ -1,16 +1,25 @@
 // Select color input
 // Select size input
-var boton = document.getElementById("send");
-console.log(boton);
-boton.addEventListener("click", function(){
-    console.log("entro en enviar");
+document.getElementById('send').addEventListener('click', function(){
+    makeGrid();
+    event.preventDefault();
 });
-
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
 // Your code goes here!
-
+    document.getElementById('pixel_canvas').innerHTML = '';
+    var width = document.getElementById('input_width').value;
+    var height = document.getElementById('input_height').value;
+    for(var i =0; i<width; i++){
+        var row = document.createElement('tr');
+        for(var j=0; j<height; j++){
+            var col = document.createElement('td');
+            row.appendChild(col);
+            col.style.borderColor = document.getElementById("colorPicker").value;
+        }
+        document.getElementById('pixel_canvas').appendChild(row);    
+    }
+    
 }
